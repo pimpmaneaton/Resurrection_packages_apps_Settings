b85f472d0d025639c539c480d1c5fcd3f6bf18fc
pimpmaneaton <shaunsibu@gmail.com>
2018-07-10 06:03:54 -0600

    Remove customer toast color options
    They just don't play well with themes
    
    Change-Id: I591c8f7863e08687371703f1b87217683a2038cd

--- src/com/android/settings/rr/animation/ToastAnimations.java
+++ src/com/android/settings/rr/animation/ToastAnimations.java
@@ -14,11 +14,8 @@
 package com.android.settings.rr.animation;
 
 import android.app.Activity;
-import android.app.ActivityManager;
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.ContentResolver;
-import android.content.DialogInterface;
 import android.provider.Settings;
 import android.os.Bundle;
 import android.support.v7.preference.ListPreference;
@@ -26,15 +23,9 @@ import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceScreen;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import java.util.ArrayList;
-import java.util.List;
 import android.widget.Toast;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
-import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -44,17 +35,7 @@ public class ToastAnimations extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "ToastAnimations";
     private static final String PREF_TOAST_ANIMATION = "toast_animation";
-    private static final String PREF_TEXT_COLOR = "toast_text_color";
-    private static final String PREF_ICON_COLOR = "toast_icon_color";
 
-    private static final int DEFAULT_COLOR_ICON = 0xffffffff;
-    private static final int DEFAULT_COLOR_TEXT = 0xff000000;
-
-    private static final int MENU_RESET = Menu.FIRST;
-    private static final int DLG_RESET = 0;
-
-    private ColorPickerPreference mIconColor;
-    private ColorPickerPreference mTextColor;
     private ListPreference mToastAnimation;
     private Context mContext;
     protected ContentResolver mContentRes;
@@ -74,31 +55,13 @@ public class ToastAnimations extends SettingsPreferenceFragment implements
         PreferenceScreen prefSet = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
 
- 	    mToastAnimation = (ListPreference) findPreference(PREF_TOAST_ANIMATION);
+ 	mToastAnimation = (ListPreference) findPreference(PREF_TOAST_ANIMATION);
         mToastAnimation.setSummary(mToastAnimation.getEntry());
         int CurrentToastAnimation = Settings.System.getInt(resolver, Settings.System.TOAST_ANIMATION, 1);
         mToastAnimation.setValueIndex(CurrentToastAnimation); //set to index of default value
         mToastAnimation.setSummary(mToastAnimation.getEntries()[CurrentToastAnimation]);
         mToastAnimation.setOnPreferenceChangeListener(this);
 
-        int intColor;
-        String hexColor;
-
-        // Toast icon color
-        mIconColor = (ColorPickerPreference) findPreference(PREF_ICON_COLOR);
-        intColor = Settings.System.getInt(resolver,
-                Settings.System.TOAST_ICON_COLOR, 0xffffffff);
-        hexColor = String.format("#%08x", (0xffffffff & intColor));
-        mIconColor.setNewPreviewColor(intColor);
-        mIconColor.setOnPreferenceChangeListener(this);
-
-        // Toast text color
-        mTextColor = (ColorPickerPreference) findPreference(PREF_TEXT_COLOR);
-        intColor = Settings.System.getInt(resolver,
-                Settings.System.TOAST_TEXT_COLOR, 0xff000000);
-        hexColor = String.format("#%08x", (0xff000000 & intColor));
-        mTextColor.setNewPreviewColor(intColor);
-        mTextColor.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -110,71 +73,7 @@ public class ToastAnimations extends SettingsPreferenceFragment implements
               mToastAnimation.setSummary(mToastAnimation.getEntries()[index]);
               Toast.makeText(mContext, "Toast Test", Toast.LENGTH_SHORT).show();
               return true;
-            }  else if (preference == mIconColor) {
-                String hex = ColorPickerPreference.convertToARGB(Integer
-                       .valueOf(String.valueOf(newValue)));
-                int intHex = ColorPickerPreference.convertToColorInt(hex);
-                Settings.System.putInt(resolver,
-                       Settings.System.TOAST_ICON_COLOR, intHex);
-                Toast.makeText(getActivity(), mToastAnimation.getEntry(),
-                       Toast.LENGTH_SHORT).show();
-                return true;
-            } else if (preference == mTextColor) {
-                String hex = ColorPickerPreference.convertToARGB(Integer
-                      .valueOf(String.valueOf(newValue)));
-                int intHex = ColorPickerPreference.convertToColorInt(hex);
-                Settings.System.putInt(resolver,
-                      Settings.System.TOAST_TEXT_COLOR, intHex);
-                Toast.makeText(getActivity(), mToastAnimation.getEntry(),
-                      Toast.LENGTH_SHORT).show();
-                return true;
         } 
         return false;
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        menu.add(0, MENU_RESET, 0, R.string.reset)
-                .setIcon(R.drawable.ic_action_reset)
-                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case MENU_RESET:
-                resetToDefault();
-                return true;
-            default:
-                return super.onContextItemSelected(item);
-        }
-    }
-
-    private void resetToDefault() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
-        alertDialog.setTitle(R.string.colors_reset_title);
-        alertDialog.setMessage(R.string.colors_reset_message);
-        alertDialog.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                resetValues();
-            }
-        });
-        alertDialog.setNegativeButton(R.string.cancel, null);
-        alertDialog.create().show();
-    }
-
-    private void resetValues() {
-	ContentResolver resolver = getActivity().getContentResolver();
-	Settings.System.putInt(resolver,
-                 Settings.System.TOAST_TEXT_COLOR, DEFAULT_COLOR_TEXT);
-        mTextColor.setNewPreviewColor(DEFAULT_COLOR_TEXT);
-        Settings.System.putInt(resolver,
-                 Settings.System.TOAST_ICON_COLOR, DEFAULT_COLOR_ICON);
-        mIconColor.setNewPreviewColor(DEFAULT_COLOR_ICON);
-	}  
 }
